fa32a97ebae8a6c942744d26621aee68ec904bb6
denizcalkan <72016288+denizcalkan@users.noreply.github.com>
2020-12-20 18:57:42 +0300

    controllers modified

--- JavaFXApplication1/src/controller/JoinController.java
+++ JavaFXApplication1/src/controller/JoinController.java
@@ -1,9 +1,16 @@
 package controller;
 
 import java.util.Iterator;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 import org.json.JSONObject; 
 import ws.client.*;
@@ -13,8 +20,21 @@ public class JoinController extends BaseController{
 
     @FXML
     private Label joinLabel;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private TextField gameCode;
     
-    
+    @FXML
+    private Rectangle rectangle;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Button ok;
 
     public JoinController( ViewFactory viewFactory, String fxmlName) {
         super( viewFactory, fxmlName);
@@ -28,11 +48,64 @@ public class JoinController extends BaseController{
         viewFactory.closeStage(stage);
     }
 
+    int isNameValid(String name){
+        if(name.length() > 14 )
+            return 0;
+        else if(name.length() <= 0)
+            return -1;
+        return 1;
+    }
+
     @FXML
     void forwardBtnAction() {
-        viewFactory.showRoomPage();
-        Stage stage = (Stage) joinLabel.getScene().getWindow();
-        viewFactory.closeStage(stage);
+
+        // Check the validity of userName
+        if( isNameValid(userName.getText()) == 0){
+           label.setText("Please enter a user name under 14 characters!");
+           makeAppear();
+        }
+        else if(isNameValid(userName.getText()) == (-1)){
+            label.setText("Please enter a user name!");
+            makeAppear();
+        }
+
+        // Store data
+        game.setUserName(userName.getText());
+        boolean isValid = game.checkGameCode(gameCode.getText());
+        boolean isFull = game.isRoomFull();
+
+        // Control validity
+        if(isValid && ! isFull) {
+            viewFactory.showRoomPage();
+            Stage stage = (Stage) joinLabel.getScene().getWindow();
+            viewFactory.closeStage(stage);
+        }
+        else if(! isValid){
+            label.setText("Game Code is not valid!");
+            makeAppear();
+        }
+        else{
+            label.setText("The room is full, sorry, next time!");
+            makeAppear();
+        }
+    }
+
+     @FXML
+    void okBtnAction(ActionEvent event) {
+        
+       rectangle.setVisible(false);
+        label.setVisible(false);
+        ok.setVisible(false);
+        ok.setDisable(true);
+    }
+    
+    
+    void makeAppear() {
+        rectangle.setVisible(true);
+        label.setVisible(true);
+        ok.setVisible(true);
+        ok.setDisable(false);
+        
     }
 
     @FXML
--- JavaFXApplication1/src/controller/RoomController.java
+++ JavaFXApplication1/src/controller/RoomController.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
@@ -15,17 +16,24 @@ import view.ViewFactory;
 public class RoomController extends BaseController{
     public RoomController( ViewFactory viewFactory, String fxmlName) {
         super( viewFactory, fxmlName);
+
+        roomLabel.setText(game.getGameCode());
+        user1.setText(game.getUserList[0]);
+        user2.setText(game.getUserList[1]);
+        user3.setText(game.getUserList[2]);
+        user4.setText(game.getUserList[3]);
+        user5.setText(game.getUserList[4]);
+        user6.setText(game.getUserList[5]);
     }
 
     @FXML
     private Label roomLabel;
     
     @FXML
-    private TextArea userList;
-    
-    public TextArea getUserList(){
-        return userList;
-    }
+    private Label user1, user2, user3, user4, user5, user6;
+
+    @FXML
+    private Button add1, add2, add3;
     
     @FXML
     void backBtnAction() {
@@ -36,6 +44,23 @@ public class RoomController extends BaseController{
     }
 
     @FXML
+    void addButton1Action(){
+      add1.setVisible(false);
+        add1.setDisable(true);
+    }
+
+@FXML
+    void addButton2Action(){
+      add2.setVisible(false);
+        add2.setDisable(true);
+    }
+
+@FXML
+    void addButton3Action(){
+      add3.setVisible(false);
+        add3.setDisable(true);
+    }
+    @FXML
     void forwardBtnAction() {
         viewFactory.showBoard();
         Stage stage = (Stage) roomLabel.getScene().getWindow();
@@ -44,9 +69,9 @@ public class RoomController extends BaseController{
 
     @FXML
     void helpBtnAction() {
-
         viewFactory.showHelp();
         Stage stage = (Stage) roomLabel.getScene().getWindow();
         stage.close();
     }
+
 }
